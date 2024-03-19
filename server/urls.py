4dff07a1ac31201c822f892630d9b626f0445f23
dhruvErryDrew <derry@drew.edu>
2024-03-18 20:50:20 -0400

    No need for ip.py

--- server/urls.py
+++ server/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import re_path, path
 from . import views
 
+host = '10.142.124.237'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('login', views.login),
